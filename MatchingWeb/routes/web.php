0e64bf5178522cb8fd03dab897b2d36d59161486
yumeji-s <timoboll810308@gmail.com>
2021-12-03 11:30:58 +0900

    thirdCommit

--- MatchingWeb/routes/web.php
+++ MatchingWeb/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\;
+use App\Http\Controllers\Test;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('index');
 });
 
-//Route::get('welcome', '');
+Route::get('welcome', [Test::class,'index']);
