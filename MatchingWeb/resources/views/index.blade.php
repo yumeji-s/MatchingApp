04710d3babcf1545bba9ffdd4b8b24ffa28e525f
yumeji-s <timoboll810308@gmail.com>
2021-12-03 15:24:03 +0900

    aaaaaa

--- MatchingWeb/resources/views/index.blade.php
+++ MatchingWeb/resources/views/index.blade.php
@@ -4,42 +4,179 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <!-- アイコン -->
+        <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('/assets/images/icon1/apple-icon-57x57.png') }}">
+        <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('/assets/images/icon1/apple-icon-60x60.png') }}">
+        <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('/assets/images/icon1/apple-icon-72x72.png') }}">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('/assets/images/icon1/apple-icon-76x76.png') }}">
+        <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('/assets/images/icon1/apple-icon-114x114.png') }}">
+        <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('/assets/images/icon1/apple-icon-120x120.png') }}">
+        <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('/assets/images/icon1/apple-icon-144x144.png') }}">
+        <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('/assets/images/icon1/apple-icon-152x152.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/assets/images/icon1/apple-icon-180x180.png') }}">
+        <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('/assets/images/icon1/android-icon-192x192.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/assets/images/icon1/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('/assets/images/icon1/favicon-96x96.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/assets/images/icon1/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('/assets/images/icon1/manifest.json') }}">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="{{ asset('/assets/images/icon1/ms-icon-144x144.png') }}">
+        <meta name="theme-color" content="#ffffff">
+
+        <title>nikoichi</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.dark\:text-gray-500{--tw-text-opacity:1;color:#6b7280;color:rgba(107,114,128,var(--tw-text-opacity))}}
-        </style>
+        <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/style.css') }}">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+        <script src="{{ asset('/assets/js/script.js') }}"></script>
 
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
     </head>
-    <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+    <body id="top">
+	<div id="wrapper">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+		<div id="sidebar">
+			<div id="sidebarWrap">
+				<h1><img src="{{ asset('/assets/images/logo.png') }}" width="87" height="25" alt="logo"></h1>
+				<nav id="mainnav">
+					<p id="menuWrap"><a id="menu"><span id="menuBtn"></span></a></p>
+					<div class="panel">
+						<ul>
+							<li><a href="#top">TOP</a></li>
+							<li><a href="#sec01">MESSAGE</a></li>
+							<li><a href="#sec02">GALLERY</a></li>
+							<li><a href="#sec03">BRAND</a></li>
+							<li><a href="#sec04">PROJECT</a></li>
+							<li><a href="#sec05">COMPANY</a></li>
+						</ul>
+						<ul id="sns">
+							<li><a href="#" target="_blank"><img src="{{ asset('/assets/images/iconFb.png') }}" width="20" height="20" alt="FB"></a></li>
+							<li><a href="#" target="_blank"><img src="{{ asset('/assets/images/iconTw.png') }}" width="20" height="20" alt="twitter"></a></li>
+							<li><a href="#" target="_blank"><img src="{{ asset('/assets/images/iconInsta.png') }}" width="20" height="20" alt="Instagram"></a></li>
+							<li><a href="#" target="_blank"><img src="{{ asset('/assets/images/iconYouTube.png') }}" width="20" height="20" alt="You Tube"></a></li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</div>
 
-            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                index画面
-            </div>
-            <a href="{{ url('welcome') }}">welcomeリンク</a>
-        </div>
-    </body>
+		<div id="content">
+			<p id="mainImg"><img src="{{ asset('/assets/images/mainImg.jpg') }}" alt=""></p>
+			<!-- MESSAGE -->
+			<section id="sec01">
+				<header>
+					<h2><span>MESSAGE</span></h2>
+				</header>
+				<div class="innerS">
+					ホームページサンプル株式会社では最新技術と自然との調和を目指します。
+                    革新的な技術で世の中を動かす企業を目指します。ホームページサンプル株式会社。
+                    ホームページサンプル株式会社では最新技術と自然との調和を目な技術で世の中を動かす企業を目指します。
+                    ホームページサンプル株式会社では最新技術と自然との調和を目指します。革新的な技術で世の中を動かす企業を目指します。
+				</div>
+			</section>
+			<!-- // MESSAGE -->
+			<!-- GALLERY -->
+			<section id="sec02">
+				<header>
+					<h2><span>GALLERY</span></h2>
+				</header>
+				<ul id="gallery">
+					<li><img src="{{ asset('/assets/images/photo01.jpg') }}" width="360" height="350" alt=""></li>
+					<li><img src="{{ asset('/assets/images/photo02.jpg') }}" width="360" height="350" alt=""></li>
+					<li><img src="{{ asset('/assets/images/photo03.jpg') }}" width="360" height="350" alt=""></li>
+					<li><img src="{{ asset('/assets/images/photo04.jpg') }}" width="360" height="350" alt=""></li>
+					<li><img src="{{ asset('/assets/images/photo05.jpg') }}" width="360" height="350" alt=""></li>
+					<li><img src="{{ asset('/assets/images/photo06.jpg') }}" width="360" height="350" alt=""></li>
+					<li class="full"><img src="{{ asset('/assets/images/photo07.jpg') }}" width="1080" height="695" alt=""></li>
+					<li><img src="{{ asset('/assets/images/photo08.jpg') }}" width="360" height="350" alt=""></li>
+					<li><img src="{{ asset('/assets/images/photo09.jpg') }}" width="360" height="350" alt=""></li>
+					<li><img src="{{ asset('/assets/images/photo10.jpg') }}" width="360" height="350" alt=""></li>
+					<li><img src="{{ asset('/assets/images/photo11.jpg') }}" width="360" height="350" alt=""></li>
+					<li><img src="{{ asset('/assets/images/photo12.jpg') }}" width="360" height="350" alt=""></li>
+					<li><img src="{{ asset('/assets/images/photo13.jpg') }}" width="360" height="350" alt=""></li>
+				</ul>
+			</section>
+			<!-- // GALLERY -->
+			<!-- BRAND -->
+			<section id="sec03">
+				<header>
+					<h2><span>BRAND</span></h2>
+				</header>
+				<div class="inner">
+					<ul class="col4">
+						<li>
+							<p class="img"><img src="{{ asset('/assets/images/logo01.png') }}" width="168" height="168" alt=""></p>
+							<p>ホームページサンプル株式会社では最動かす企業を目指します。</p>
+						</li>
+						<li>
+							<p class="img"><img src="{{ asset('/assets/images/logo02.png') }}" width="168" height="168" alt=""></p>
+							<p>革新的な技術で世の中を動かす企業を目します。世の中を動かす。</p>
+						</li>
+						<li>
+							<p class="img"><img src="{{ asset('/assets/images/logo03.png') }}" width="168" height="168" alt=""></p>
+							<p>株式会社では最動かす企業を目指しますージン企業を目指します。</p>
+						</li>
+						<li>
+							<p class="img"><img src="{{ asset('/assets/images/logo04.png') }}" width="168" height="168" alt=""></p>
+							<p>株式会社では最動かす企業を指しますージサン企業を目指します。</p>
+						</li>
+					</ul>
+				</div>
+			</section>
+			<!-- // BRAND -->
+			<!-- PROJECT -->
+			<section id="sec04">
+				<header>
+					<h2><span>PROJECT</span></h2>
+				</header>
+				<div class="inner">
+					<div class="article">
+						<img src="{{ asset('/assets/images/photo14.jpg') }}" width="370" height="224" alt="">
+						<p>ホームページサンプル株式会社では最新技術と自然との調和を目指します。革新的な革新的な革新的な技術で世の中を技術で世の中を技術で世の中を動かす企業を目指します。</p>
+						<p>ホームページサンプル株式会社。ホームページサンプル最新技術と自然との調和を目指します。革新的な技術で世の中を動かす企業を目指します。ホームページサンプル株式会社。ホームページサンプル最新技術と自然との調和を目指し調和を目指し調和を目指します。革新的な技術で世の中を動かす企業を目指します。</p>
+					</div>
+					<div class="article">
+						<img src="{{ asset('/assets/images/photo15.jpg') }}" width="370" height="224" alt="">
+						<p>ホームページサンプル株式会社では最新技術と自然との調和を目指します。革新的な革新的な革新的な技術で世の中を技術で世の中を技術で世の中を動かす企業を目指します。</p>
+						<p>ホームページサンプル株式会社。ホームページサンプル最新技術と自然との調和を目指します。革新的な技術で世の中を動かす企業を目指します。ホームページサンプル株式会社。ホームページサンプル最新技術と自然との調和を目指し調和を目指し調和を目指します。革新的な技術で世の中を動かす企業を目指します。</p>
+					</div>
+				</div>
+			</section>
+			<!-- // PROJECT -->
+			<!-- COMPANY -->
+			<section id="sec05">
+				<header>
+					<h2><span>COMPANY</span></h2>
+				</header>
+				<div class="inner">
+					<ul class="col2">
+						<li><!--
+							<p>〒989-1102<br>-1</p>
+							<p>TEL 0224-35-1511</p>
+							 <p>公式Webサイト <a href="https://www.pref.miyagi.jp/soshiki/srkogsn/">https://www.pref.miyagi.jp/soshiki/srkogsn/</a></p>
+							<p>営業時間 09:00〜16:00（土日休み）</p>
+							<p>※都合により休校する場合がございます</p>
+						</li> -->
+						<li>
+							<div id="map">
+								<!-- GOOGLE MAP -->
+								
+								<!-- // GOOGLE MAP -->
+							</div>
+						</li>
+					</ul>
+				</div>
+			</section>
+			<!-- // COMPANY -->
+
+			<footer id="footer">
+				Copyright(c) 2021 Mecha WHITE Na Kigyou All Rights Reserved. Design by <a href="http://f-tpl.com" target="_blank">http://f-tpl.com</a><!-- ←クレジット表記を外す場合はシリアルキーが必要です http://f-tpl.com/credit/ -->
+			</footer>
+
+		</div>
+	</div>
+
+</body>
 </html>
